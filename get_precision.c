4a7a5340280276f94960a3110ebaf9c6e2e807bc
kassimu21 <sksallah45@gmail.com>
2022-10-17 11:11:52 -0700

    printf files

--- get_precision.c
+++ get_precision.c
@@ -2,9 +2,9 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed
- * @list: list of arguments
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
  * Return: Precision.
  */
